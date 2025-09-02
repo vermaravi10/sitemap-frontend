f8c46deb15d4afd7ea4b357dd205c6ef178ff7d6
Ravi Verma <admin@YIP1214.local>
2025-09-02 16:13:06 +0530

    removed store

--- src/store/hooks.ts
+++
@@ -1,7 +0,0 @@
-// src/store/hooks.ts
-import { useDispatch, useSelector } from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux";
-import type { RootState, AppDispatch } from "./store";
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
